c8d9d5ae6ac69ecc2f589bc0c20345dcab623575
Jihyeong Ko <silkstaff@naver.com>
2021-01-19 18:17:55 +0900

    solved text_processing

--- text_processing.py
+++ text_processing.py
@@ -6,9 +6,9 @@
 NLP에서 흔히하는 전처리는 소문자 변환, 앞뒤 필요없는 띄어쓰기를 제거하는 등의 텍스트 정규화 (text normalization)입니다. 
 이번 숙제에서는 텍스트 처리 방법을 파이썬으로 배워보겠습니다. 
 """
+import re
 
-
-def normalize(input_string):
+def normalize(input_string: str) -> str:
     """
      인풋으로 받는 스트링에서 정규화된 스트링을 반환함
      아래의 요건들을 충족시켜야함
@@ -33,7 +33,11 @@ def normalize(input_string):
              >>> tp.normalize(input_string2)
              'extra space'
     """
-    normalized_string = None
+    parsed_string = input_string.split(" ")
+    normalized_string_parsed = list(
+        map(lambda x: x.lower(), filter(lambda x: len(x) > 0, parsed_string))
+    )
+    normalized_string = " ".join(normalized_string_parsed)
     return normalized_string
 
 
@@ -58,5 +62,17 @@ def no_vowels(input_string):
             >>> tp.normalize(input_string2)
             ''W lv Pythn!'
     """
-    no_vowel_string = None
+    vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    NONE = ''
+    no_vowel_string = input_string[:]
+
+    for v in vowel:
+        if v in no_vowel_string:
+            no_vowel_string = no_vowel_string.replace(v, NONE)
+
     return no_vowel_string
+
+#%%
+a = 'asdfasdf'
+a.replace('a', '')
+a
